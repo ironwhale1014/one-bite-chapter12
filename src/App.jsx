925ab641283410416e039f272a894e83036019b7
King <mac@mac.com>
2024-08-17 23:50:55 +0900

    router path

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 import './App.css'
+import Home from "./pages/Home.jsx";
+import {Route, Routes} from "react-router-dom";
+import Diary from "./pages/Diary.jsx";
+import NotFoundPage from "./pages/NotFound.jsx";
+import New from "./pages/New.jsx";
+import Edit from "./pages/Edit.jsx";
 
 function App() {
 
-  return (
-    <>
-
-    </>
-  )
+    return (
+        <>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/diary/:id" element={<Diary/>}/>
+                <Route path="/new" element={<New/>}/>
+                <Route path="/Edit/:id" element={<Edit/>}/>
+                <Route path="/*" element={<NotFoundPage/>}/>
+            </Routes>
+        </>
+    )
 }
 
 export default App
---
+++ src/pages/Diary.jsx
@@ -0,0 +1,11 @@
+import {useParams} from "react-router-dom";
+
+const Diary = () => {
+    const params = useParams()
+
+    return (<div>
+        {`${params.id} 번째 일기`}
+    </div>);
+}
+
+export default Diary;
