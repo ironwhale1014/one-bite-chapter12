6187f6c3ef383b4c03d65641af8512ce880fc24a
King <mac@mac.com>
2024-08-17 23:53:31 +0900

    mockData

---
+++ src/constants/mockData.jsx
@@ -0,0 +1,7 @@
+export const mockData = [{
+    id: 1, createdDate: new Date("2024-02-19").getTime(), emotionId: 1, content: "1번 일기 내용",
+}, {
+    id: 2, createdDate: new Date("2024-02-18").getTime(), emotionId: 2, content: "2번 일기 내용",
+}, {
+    id: 3, createdDate: new Date("2024-01-07").getTime(), emotionId: 3, content: "3번 일기 내용",
+},];
